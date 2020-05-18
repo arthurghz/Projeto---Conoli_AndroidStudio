3cd866d7756aff646a82c53ab09b46c5aee03826
joaoGabriel <j199617@dac.unicamp.br>
2020-05-18 14:26:06 -0300

    Corrigido problema de direcionamento de cadastro

--- projetoConoli/app/src/main/java/br/unicamp/ft/a213281_j199617/conoli/ui/Imoveis.java
+++ projetoConoli/app/src/main/java/br/unicamp/ft/a213281_j199617/conoli/ui/Imoveis.java
@@ -3,12 +3,15 @@ package br.unicamp.ft.a213281_j199617.conoli.ui;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import br.unicamp.ft.a213281_j199617.conoli.R;
 import br.unicamp.ft.a213281_j199617.conoli.cadastro_imovel;
@@ -17,6 +20,7 @@ import br.unicamp.ft.a213281_j199617.conoli.cadastro_imovel;
  * A simple {@link Fragment} subclass.
  */
 public class Imoveis extends Fragment {
+
 
     public Imoveis() {
         // Required empty public constructor
@@ -28,12 +32,18 @@ public class Imoveis extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_imoveis, container, false);
 
+
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        getView().findViewById(R.id.batataCadastro).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getContext(), cadastro_imovel.class);
+                startActivity(intent1);
+            }
+        });
 
-    public void cadastro(View View){
-        Intent intent1 = new Intent(getContext(), cadastro_imovel.class);
-        startActivity(intent1);
     }
-
 }
