0190e4bfbb804a2ae50a185eabf5ef8ddc5c911a
joaoGabriel <j199617@dac.unicamp.br>
2020-06-22 08:19:24 -0300

    registrarImovel realiza inserções no Firestorage 100%

--- projetoConoli/app/src/main/java/br/unicamp/ft/a213281_j199617/conoli/cadastro_imovel.java
+++ projetoConoli/app/src/main/java/br/unicamp/ft/a213281_j199617/conoli/cadastro_imovel.java
@@ -1,22 +1,38 @@
 package br.unicamp.ft.a213281_j199617.conoli;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.gson.JsonArray;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import br.unicamp.ft.a213281_j199617.conoli.ui.home.HomeFragment;
 
 
 public class cadastro_imovel extends AppCompatActivity {
 
+    private static final String TAG = "STATUS_INSERT";
+    FirebaseAuth mAuth;
+
+    // Accessar a cloud firestore a partir do formulário de cadastro
     FirebaseFirestore db = FirebaseFirestore.getInstance(); // objeto Firestore
 
     private RadioGroup admImovel, tipoImovel, tipoVaga, tipoQuarto;
@@ -28,6 +44,8 @@ public class cadastro_imovel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_imovel);
+
+        mAuth = FirebaseAuth.getInstance();
 
         admImovel      = findViewById(R.id.radioGroupAdmImovel);
         tipoImovel     = findViewById(R.id.radioGroupTipoImovel);
@@ -59,15 +77,15 @@ public class cadastro_imovel extends AppCompatActivity {
         valorAdmImovel = (String) selectedRadioButtonAdm.getText();
 
         int idTipoImovel = tipoImovel.getCheckedRadioButtonId();
-        RadioButton selectedRadioButtonTipoImovel = (RadioButton) findViewById(idAdmImovel);
+        RadioButton selectedRadioButtonTipoImovel = (RadioButton) findViewById(idTipoImovel);
         valorTipoImovel = (String) selectedRadioButtonTipoImovel.getText();
 
         int idTipoVaga = tipoVaga.getCheckedRadioButtonId();
-        RadioButton selectedRadioButtonTipoVaga = (RadioButton) findViewById(idAdmImovel);
+        RadioButton selectedRadioButtonTipoVaga = (RadioButton) findViewById(idTipoVaga);
         valorTipoVaga = (String) selectedRadioButtonTipoVaga.getText();
 
         int idTipoQuarto = tipoQuarto.getCheckedRadioButtonId();
-        RadioButton selectedRadioButtonTipoQuarto = (RadioButton) findViewById(idAdmImovel);
+        RadioButton selectedRadioButtonTipoQuarto = (RadioButton) findViewById(idTipoQuarto);
         valorTipoQuarto = (String) selectedRadioButtonTipoQuarto.getText();
 
         boolean checkboxGeladeira = geladeira.isChecked();
@@ -82,6 +100,51 @@ public class cadastro_imovel extends AppCompatActivity {
         boolean checkboxInterfone = interfone.isChecked();
         boolean checkboxArCondicionado = arcondicionado.isChecked();
         boolean checkboxVaranda = varanda.isChecked();
+
+        Map<String, Object> imovel = new HashMap<>();
+        imovel.put("usuario_dono", Objects.requireNonNull(mAuth.getCurrentUser()).getUid());
+        imovel.put("administracao", valorAdmImovel);
+        imovel.put("tipo_imovel", valorTipoImovel);
+        imovel.put("tipo_vaga", valorTipoVaga);
+        imovel.put("tipo_quarto", valorTipoQuarto);
+        imovel.put("geladeira", checkboxGeladeira);
+        imovel.put("maquina_de_lavar", checkboxMaquinaDeLavar);
+        imovel.put("fogao", checkboxFogao);
+        imovel.put("microondas", checkboxMicroondas);
+        imovel.put("televisao", checkboxTelevisao);
+        imovel.put("wifi", checkboxWifi);
+        imovel.put("garagem", checkboxGaragem);
+        imovel.put("mobilia", checkboxMobilia);
+        imovel.put("utensilio", checkboxUtensilio);
+        imovel.put("interfone", checkboxInterfone);
+        imovel.put("ar_condicionado", checkboxArCondicionado);
+        imovel.put("varanda", checkboxVaranda);
+
+
+        // Add a new document with a generated ID
+        db.collection("imoveis")
+                .add(imovel)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "Documento adicionado com o ID: " + documentReference.getId());
+
+                        Toast.makeText(getApplicationContext(),
+                                "Imóvel cadastrado com sucesso!", Toast.LENGTH_LONG).show();
+
+                        Intent navigation = new Intent(getApplicationContext(), navigationDrawer.class);
+                        startActivity(navigation);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+
+                        Toast.makeText(getApplicationContext(),
+                                "Por favor, verifique os campos e tente novamente", Toast.LENGTH_LONG).show();
+                    }
+                });
 
     }
 
